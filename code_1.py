cbad9d96101b0155aee297768ca3e8e4cea5915f
Jack <143327277+Jack30046@users.noreply.github.com>
2023-12-10 06:00:14 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+function main() {
+
+    const data = generateRandomData();
+    return data;
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = [];
+function generateRandomData() {
+}
+}
+    });
+    data.forEach(item => {
+main();
+
+    }
+    for (let i = 0; i < 10; i++) {}
+    data.forEach(item => {
+
+    }
+    return data;
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    for (let i = 0; i < 10; i++) {
+function main() {
+main();
+}
+    const data = generateRandomData();
+
+    });
+    const data = [];
+        console.log("Random Number: " + item);
